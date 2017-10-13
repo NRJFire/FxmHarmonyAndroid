f2390d7a9ffdd48c07967f2c23ed208bbc038b35
Maxim <maxshelud@gmail.com>
2017-10-13 15:08:17 +0300

    Cleaned push service, add push icon

--- app/src/main/java/com/sofac/fxmharmony/util/googleFirebaseService/MyFirebaseMessagingService.java
+++ app/src/main/java/com/sofac/fxmharmony/util/googleFirebaseService/MyFirebaseMessagingService.java
@@ -27,33 +27,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     NotificationManager mNotificationManager;
     String pushMessageType = "";
 
-
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-         pushMessageType = remoteMessage.getData().get("type");
+        Timber.e("onMessageReceived -> " + remoteMessage.getData());
 
-
-        if (Constants.GROUP_PUSH_TYPE.equals(pushMessageType)){
+        if (remoteMessage.getData().size() > 0) {
             buildNotificationToShow(remoteMessage.getData().get("message"), remoteMessage.getData().get("date"), remoteMessage.getData().get("title"));
-        } else {
 
-            if (remoteMessage.getData().size() > 0) {
-                buildNotificationToShow(remoteMessage.getData().get("message"), remoteMessage.getData().get("date"), remoteMessage.getData().get("title"));
-
-                PushMessage newPushMessage = new PushMessage(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"), remoteMessage.getData().get("date"));
-                newPushMessage.save();
-            }
-
-            if (remoteMessage.getNotification() != null) {
-                Timber.i("Message Notification Body: " + remoteMessage.getNotification().getBody());
-            }
+            PushMessage newPushMessage = new PushMessage(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"), remoteMessage.getData().get("date"));
+            newPushMessage.save();
         }
+
+        if (remoteMessage.getNotification() != null) {
+            Timber.i("Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
     }
 
 
     private void buildNotificationToShow(String messageText, String date, String title) {
-
 
         Intent notificationIntent = null;
         notificationIntent = new Intent(this, SplashActivity.class);
@@ -63,7 +55,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         builder = new NotificationCompat.Builder(this);
         builder.setContentTitle(title);
         builder.setContentText(Html.fromHtml(messageText).toString())
-                .setSmallIcon(R.drawable.icon)
+                .setSmallIcon(getNotificationIcon())
               /*  .setStyle(bigPictureStyle)*/
                 .setAutoCancel(true)
                 .setContentIntent(
@@ -76,6 +68,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         mNotificationManager.notify((int) (Math.random() * 100000), builder.build());
 
+    }
+
+    private int getNotificationIcon() {
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.drawable.ic_stat_icon_silhouette : R.drawable.icon;
     }
 
 }
