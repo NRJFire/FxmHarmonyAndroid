1c32e2863b49ecfcfae31cbc62e7c3aa5037274c
Maxim <maxshelud@gmail.com>
2017-11-30 18:05:46 +0200

    Changing tabs xml-s

--- app/src/main/java/com/sofac/fxmharmony/view/DetailTossActivity.java
+++ app/src/main/java/com/sofac/fxmharmony/view/DetailTossActivity.java
@@ -2,14 +2,20 @@ package com.sofac.fxmharmony.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.sofac.fxmharmony.R;
 
 public class DetailTossActivity extends AppCompatActivity {
 
+    public Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_toss);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
